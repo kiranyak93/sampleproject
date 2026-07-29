55315c094b01595587eb23ca73b0cdfbbc4cdf1c
kiranyak93 <kiranyakaruna@gmail.com>
2026-07-29 23:15:57 +0530

    function

---
+++ basic4.js
@@ -0,0 +1,25 @@
+function greet(){
+    console.log("welcome to javascrpit");
+}
+greet();
+function greet1(name){
+    console.log(name);
+}
+greet1("Anajli");
+
+function add(a,b){
+    return a+b;
+    
+}
+let result=add(10,20);
+console.log(result);
+
+
+function square(number){
+    return number*number;
+}
+console.log(square(5));function sample(){
+    let x=10;
+}
+
+
