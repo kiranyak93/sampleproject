ccb335bcfb1d22843896c3d9afeb2d12b4b08b2f
kiranyak93 <kiranyakaruna@gmail.com>
2026-07-29 23:12:58 +0530

    datatypes

---
+++ basic3.js
@@ -0,0 +1,22 @@
+let studentname="Nimmy";
+console.log(typeof studentname);
+let mark=95;
+console.log(typeof mark);
+let ispassed=true;
+console.log(typeof ispassed);
+let age;
+console.log(typeof age);
+let salary=null;
+console.log(typeof salary);
+let students=["java","JS","Playwright"];
+console. log(typeof students);
+let employee={
+    id:111,
+    name:"John",
+    department:"QA"
+
+};
+console.log(employee);
+let id1=Symbol("employee");
+let id2=Symbol("employee");
+console.log(id1===id2);
