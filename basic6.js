9e33d51b35edc281d1aec068d91fff40485587b7
kiranyak93 <kiranyakaruna@gmail.com>
2026-08-01 20:55:38 +0530

    seventh

---
+++ basic6.js
@@ -0,0 +1,21 @@
+const welcome=()=>{
+    console.log("welcome");
+
+}
+welcome();
+
+const square=num=>{
+    return num*num;
+     
+}
+console.log(square(5));
+
+
+const add=(a,b)=>{
+    return a+b;
+}
+console.log(add(1,2));
+
+
+const add1=(a,b)=>a+b;
+console.log(add1(2,3));
