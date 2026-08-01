5b9f6e641453d3f1f5eeabb9cf94707f17c6cf0e
kiranyak93 <kiranyakaruna@gmail.com>
2026-08-01 20:56:49 +0530

    seventh

---
+++ basic8.js
@@ -0,0 +1,23 @@
+let boys=["ajay","manoj"];
+let girls=["anju","anjali"];
+//for joint two array
+let student=boys.concat(girls);
+console.log(student);
+
+let number=[10,20,30,40,50];
+//return a portion of array
+let result=[number.slice(1,5)];
+console.log(result);
+
+let fruit=["apple","mango"];
+//reverse an array
+fruit.reverse();
+console.log(fruit);
+//for sorting
+fruit.sort();
+console.log(fruit);
+//for removing elements
+
+let number1=[10,20,30,40,50];
+number1.splice(1,2);
+console.log(number1);
