5b12925995bdb4a722d554a4a4f7681afc719dbc
kiranyak93 <kiranyakaruna@gmail.com>
2026-08-06 12:12:49 +0530

    maparray

---
+++ basic10.js
@@ -0,0 +1,14 @@
+//map array mathod
+
+let numbers=[1,2,3,4,5];
+let result= numbers.map(num =>num*2);
+console.log(result);
+
+//map filter method
+let number=[1,2,3,4,5,6];
+let result1= number.filter(num =>num%2==0);
+console.log(result1);
+
+let marks=[10,20,30,40,50];
+let result2= marks.filter(mark =>mark >20);
+console.log(result2);
