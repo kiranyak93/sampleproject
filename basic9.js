e835685fc4a35c66604b7931ae314431b3dd753b
kiranyak93 <kiranyakaruna@gmail.com>
2026-08-06 12:07:00 +0530

    fifth

---
+++ basic9.js
@@ -0,0 +1,18 @@
+//Array Menthods
+// foreach method syntax
+//Array.array.forEach((element,index)=>{
+
+
+    
+//});
+
+const number=[10,20,30];
+number.forEach((num)=>{
+    console.log(num)
+})
+
+const fruit=["apple","mango","grapes"];
+fruit.forEach((fruits,index)=>{
+    console.log(index ,+""+fruits);
+})
+
